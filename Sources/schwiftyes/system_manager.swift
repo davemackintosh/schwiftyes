9f35e82e34e612ad571dffc6431418521a27e437
davemackintosh <me@davemackintosh.co.uk>
2023-02-21 14:33:59 +0000

    You destended rectal cavity.

--- Sources/schwiftyes/system_manager.swift
+++ Sources/schwiftyes/system_manager.swift
@@ -2,7 +2,7 @@ import Foundation
 
 open class System<Signatures: OptionSet> {
     var signature: Signatures { fatalError("Must override a system's signatures otherwise it won't run on any entities.") }
-    var entities: [Entity] = []
+    public var entities: [Entity] = []
     public var componentManager: ComponentManager<Signatures>!
 
     public required init(_ componentManager: ComponentManager<Signatures>) {
